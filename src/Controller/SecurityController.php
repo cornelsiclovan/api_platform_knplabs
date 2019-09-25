f71780915e0b85221e3a997e842a80fecd32f268
cornel <cornel.siclovan@gmail.com>
2019-09-25 11:24:16 +0300

    initial commit

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 24.09.2019
+ * Time: 15:44
+ */
+
+namespace App\Controller;
+use ApiPlatform\Core\Api\IriConverterInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login", methods={"POST"})
+     */
+    public function login(IriConverterInterface $iriConverter)
+    {
+        if(!$this->isGranted('IS_AUTHENTICATED_FULLY')){
+            return $this->json([
+                'error' => 'Invalid login request: check that the Content-Type header is "application/json"'
+            ], 400);
+        }
+
+
+        return new Response(null, 204,  [
+            'Location' => $iriConverter->getIriFromItem($this->getUser())
+        ]);
+    }
+
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \Exception('should not be reached');
+    }
+}
